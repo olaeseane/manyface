4c7fd9463e111d427e79d7b6370db6c3a3fe4850
olaeseane <olaesean@gmail.com>
2021-12-05 00:22:57 +0300

    chore: Change folders/files location

--- configs/script.sh
+++
@@ -1,7 +0,0 @@
-ssh -i manyface-server.cer ec2-user@ec2-35-159-23-84.eu-central-1.compute.amazonaws.com
-scp -i manyface-server.cer ../cmd/server/server  ec2-user@ec2-35-159-23-84.eu-central-1.compute.amazonaws.com:/home/ec2-user
-
-ngrok http --region=eu --hostname=manyface.eu.ngrok.io 8080
-ngrok tcp --region=eu --remote-addr=3.tcp.eu.ngrok.io:23623 5300
-
-./client -ws=http://manyface.eu.ngrok.io -gs=3.tcp.eu.ngrok.io:23623 -u=user1 -p=welcome
